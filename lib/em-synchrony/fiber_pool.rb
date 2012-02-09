0ce62a1b15b1de369c780ac1748749104f84b74a
Andrew rudenko <ceo@prepor.ru>
2012-02-09 21:11:04 +0400

    fixed fiber pool dependencies!

--- lib/em-synchrony/fiber_pool.rb
+++ lib/em-synchrony/fiber_pool.rb
@@ -1,8 +1,8 @@
 # Author::    Mohammad A. Ali  (mailto:oldmoe@gmail.com)
 # Copyright:: Copyright (c) 2008 eSpace, Inc.
 # License::   Distributes under the same terms as Ruby
-require 'em-sycnhrony/event'
-require 'em-sycnhrony/group'
+require 'em-synchrony/event'
+require 'em-synchrony/group'
 module EM::Synchrony
 
   class FiberPool
